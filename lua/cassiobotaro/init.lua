80f3a249365ac9ab82d64c9dcee0df13c2668e45
cassiobotaro <cassiobotaro@gmail.com>
2022-12-26 11:30:34 -0300

    Format files using stylua

--- lua/cassiobotaro/init.lua
+++ lua/cassiobotaro/init.lua
@@ -1,5 +1,5 @@
-require('cassiobotaro.packer')
-require('cassiobotaro.global')
-require('cassiobotaro.option')
-require('cassiobotaro.remap')
-require('cassiobotaro.autocommand')
+require 'cassiobotaro.packer'
+require 'cassiobotaro.global'
+require 'cassiobotaro.option'
+require 'cassiobotaro.remap'
+require 'cassiobotaro.autocommand'
