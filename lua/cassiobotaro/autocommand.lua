506f6edb0e888e1cdca8d36047aa244de881859c
cassiobotaro <cassiobotaro@gmail.com>
2023-07-18 08:26:31 -0300

    Update neotree

--- lua/cassiobotaro/autocommand.lua
+++ lua/cassiobotaro/autocommand.lua
@@ -1,22 +1,22 @@
 vim.api.nvim_create_autocmd('BufReadPost', {
-  callback = function()
-    local row, col = unpack(vim.api.nvim_buf_get_mark(0, '"'))
-    if row > 0 and row <= vim.api.nvim_buf_line_count(0) then
-      vim.api.nvim_win_set_cursor(0, { row, col })
-    end
-  end,
+    callback = function()
+        local row, col = unpack(vim.api.nvim_buf_get_mark(0, '"'))
+        if row > 0 and row <= vim.api.nvim_buf_line_count(0) then
+            vim.api.nvim_win_set_cursor(0, { row, col })
+        end
+    end,
 })
 
 -- remove trailing whitespace on save
 vim.api.nvim_create_autocmd('BufWritePre', {
-  callback = function()
-    vim.cmd ':%s/\\s\\+$//e'
-  end,
+    callback = function()
+        vim.cmd ':%s/\\s\\+$//e'
+    end,
 })
 
 -- highlight on yank
 vim.api.nvim_create_autocmd('TextYankPost', {
-  callback = function()
-    vim.highlight.on_yank()
-  end,
+    callback = function()
+        vim.highlight.on_yank()
+    end,
 })
--- lua/plugins/neo-tree.lua
+++ lua/plugins/neo-tree.lua
@@ -1,19 +1,22 @@
 -- FIle explorer
 return {
-  'nvim-neo-tree/neo-tree.nvim',
-  cmd = 'NeoTree',
-  branch = 'v2.x',
-  keys = {
-    { '<leader>ft', '<cmd>Neotree toggle<cr>', desc = 'Neotree' },
-  },
-  dependencies = {
-    'nvim-lua/plenary.nvim',
-    'MunifTanjim/nui.nvim',
-  },
-  opts = {
-    filesystem = {
-      follow_current_file = true,
-      hijack_netrw_behavior = 'open_current',
+    'nvim-neo-tree/neo-tree.nvim',
+    cmd = 'NeoTree',
+    branch = 'v3.x',
+    keys = {
+        { '<leader>ft', '<cmd>Neotree toggle<cr>', desc = 'Neotree' },
     },
-  },
+    dependencies = {
+        'nvim-lua/plenary.nvim',
+        'MunifTanjim/nui.nvim',
+        'nvim-tree/nvim-web-devicons',
+    },
+    opts = {
+        filesystem = {
+            follow_current_file = {
+                enabled = true,
+            },
+            hijack_netrw_behavior = 'open_current',
+        },
+    },
 }
