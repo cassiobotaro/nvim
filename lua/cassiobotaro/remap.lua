6a68602389a6a599459988adb4ac8761e28595ab
cassiobotaro <cassiobotaro@gmail.com>
2023-09-28 13:56:52 -0300

    Add descriptions to remaps

--- lua/cassiobotaro/remap.lua
+++ lua/cassiobotaro/remap.lua
@@ -1,21 +1,21 @@
-vim.keymap.set('v', 'J', [[:m '>+1<CR>gv=gv]]) -- move lines up respecting identation
-vim.keymap.set('v', 'K', [[:m '<-2<CR>gv=gv]]) -- move lines down respecting identation
-vim.keymap.set('n', 'J', [[mzJ`z]]) -- join lines without move cursor
-vim.keymap.set('n', '<C-d>', [[<C-d>zz]]) -- scroll window Downwards in the buffer.
-vim.keymap.set('n', '<C-u>', [[<C-u>zz]]) -- scroll window Upwards in the buffer.
-vim.keymap.set('n', 'n', [[nzzzv]]) -- search will center on the line it's found in
-vim.keymap.set('n', 'N', [[Nzzzv]]) -- search will center on the line it's found in
-vim.keymap.set('n', '<C-k>', [[<cmd>cnext<CR>zz]]) -- navigate next in quickfix list
-vim.keymap.set('n', '<C-j>', [[<cmd>cprev<CR>zz]]) -- navigate previous in quickfix list
-vim.keymap.set('n', '<leader>k', [[<cmd>lnext<CR>zz]]) -- navigate next in location list
-vim.keymap.set('n', '<leader>j', [[<cmd>lprev<CR>zz]]) -- navigate previous in location list
-vim.keymap.set('n', '<leader>r', [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]]) -- search and replace
-vim.keymap.set('n', '<leader>x', [[<cmd>!chmod +x %<CR>]], { silent = true }) -- allow execution permission
-vim.keymap.set('n', '<leader>h', vim.cmd.split) -- split horizontally
-vim.keymap.set('n', '<leader>v', vim.cmd.vsplit) -- split vertically
-vim.keymap.set('n', '<leader>q', vim.cmd.bp) -- move to previous buffer
-vim.keymap.set('n', '<leader>w', vim.cmd.bn) -- move to next buffer
-vim.keymap.set('n', '<leader>c', vim.cmd.bd) -- close current buffer
-vim.keymap.set('n', '<leader>.', [[<Cmd>lcd %:p:h<CR>]]) -- set current file directory as working directory
-vim.keymap.set('v', '<', [[<gv]]) -- move code forward in visual mode
-vim.keymap.set('v', '>', [[>gv]]) -- move code backward in visual mode
+vim.keymap.set('v', 'J', [[:m '>+1<CR>gv=gv]], { desc = 'Move lines up respecting identation' })
+vim.keymap.set('v', 'K', [[:m '<-2<CR>gv=gv]], { desc = 'Move lines down respecting identation' })
+vim.keymap.set('n', 'J', [[mzJ`z]], { desc = 'Join lines without move cursor' })
+vim.keymap.set('n', '<C-d>', [[<C-d>zz]], { desc = 'Scroll window downwards in the buffer' })
+vim.keymap.set('n', '<C-u>', [[<C-u>zz]], { desc = 'Scroll window upwards in the buffer' })
+vim.keymap.set('n', 'n', [[nzzzv]], { desc = "Search will center on the line it's found in" })
+vim.keymap.set('n', 'N', [[Nzzzv]], { desc = "Search will center on the line it's found in" })
+vim.keymap.set('n', '<C-k>', [[<cmd>cnext<CR>zz]], { desc = 'Navigate next in quickfix list' })
+vim.keymap.set('n', '<C-j>', [[<cmd>cprev<CR>zz]], { desc = 'Navigate previous in quickfix list' })
+vim.keymap.set('n', '<leader>k', [[<cmd>lnext<CR>zz]], { desc = 'Navigate next in location list' })
+vim.keymap.set('n', '<leader>j', [[<cmd>lprev<CR>zz]], { desc = 'Navigate previous in location list' })
+vim.keymap.set('n', '<leader>r', [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]], { desc = 'Search and replace' })
+vim.keymap.set('n', '<leader>x', [[<cmd>!chmod +x %<CR>]], { silent = true }, { desc = 'Allow execution permission' })
+vim.keymap.set('n', '<leader>h', vim.cmd.split, { desc = 'Split horizontally' })
+vim.keymap.set('n', '<leader>v', vim.cmd.vsplit, { desc = 'Split vertically' })
+vim.keymap.set('n', '<leader>q', vim.cmd.bp, { desc = 'Move to previous buffer' })
+vim.keymap.set('n', '<leader>w', vim.cmd.bn, { desc = 'Move to next buffer' })
+vim.keymap.set('n', '<leader>c', vim.cmd.bd, { desc = 'Close current buffer' })
+vim.keymap.set('n', '<leader>.', [[<Cmd>lcd %:p:h<CR>]], { desc = 'Set current file directory as working directory' })
+vim.keymap.set('v', '<', [[<gv]], { desc = 'Move code forward in visual mode' })
+vim.keymap.set('v', '>', [[>gv]], { desc = 'Move code backward in visual mode' })
