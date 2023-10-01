4ae25f1569a1fdc20b76337552fa6aa739e1dbfe
cassiobotaro <cassiobotaro@gmail.com>
2023-10-01 20:57:22 -0300

    Switch from null-ls to none-ls

---
+++ lua/plugins/none-ls.lua
@@ -0,0 +1,52 @@
+return {
+    'nvimtools/none-ls.nvim',
+    dependencies = {
+        { 'jay-babu/mason-null-ls.nvim' },
+        { 'williamboman/mason.nvim' },
+    },
+    config = function()
+        local nls = require 'null-ls'
+        require('mason-null-ls').setup {
+            ensure_installed = {
+                'stylua',
+                'black',
+                'prettier',
+                'shfmt',
+                'goimports',
+                'shfmt',
+                'yamllint',
+                'protolint',
+                'ruff_lsp',
+            },
+        }
+
+        -- configuring null-ls for formatters
+        local formatting = nls.builtins.formatting
+        local diagnostics = nls.builtins.diagnostics
+        local actions = nls.builtins.code_actions
+
+        nls.setup {
+            sources = {
+                formatting.prettier.with {
+                    filetypes = { 'json', 'markdown', 'toml' },
+                },
+                formatting.shfmt,
+                formatting.stylua.with {
+                    condition = function(utils)
+                        return utils.root_has_file { 'stylua.toml', '.stylua.toml' }
+                    end,
+                },
+                formatting.goimports,
+                formatting.black,
+                diagnostics.yamllint.with {
+                    extra_args = { '-d', '{extends: relaxed, rules: {line-length: {max: 200}}}' },
+                },
+                diagnostics.shellcheck,
+                actions.shellcheck,
+                formatting.protolint,
+                formatting.ruff,
+                diagnostics.protolint,
+            },
+        }
+    end,
+}
