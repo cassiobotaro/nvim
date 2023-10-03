2890fc12529ed4e8484849a9171c4a3548b8634d
cassiobotaro <cassiobotaro@gmail.com>
2023-10-02 22:54:25 -0300

    Remove which-key

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -2,7 +2,11 @@
 return {
   'nvim-telescope/telescope.nvim',
   branch = '0.1.x',
-  dependencies = { 'nvim-lua/plenary.nvim' },
+  dependencies = {
+    { 'nvim-lua/plenary.nvim' },
+    { 'nvim-telescope/telescope-fzf-native.nvim', build = 'make' },
+    { 'nvim-telescope/telescope-ui-select.nvim' },
+  },
   config = function()
     local builtin = require 'telescope.builtin'
     -- See `:help telescope.builtin`
