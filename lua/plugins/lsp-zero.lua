5ad47cba85022a998eefd308206811aec4a6c161
cassiobotaro <cassiobotaro@gmail.com>
2023-10-30 22:21:29 -0300

    fix lsp

--- lua/plugins/lsp-zero.lua
+++ lua/plugins/lsp-zero.lua
@@ -69,13 +69,6 @@ return {
       },
       handlers = {
         lsp_zero.default_setup,
-      },
-    }
-    require('mason').setup {}
-    require('mason-lspconfig').setup {
-      ensure_installed = {},
-      handlers = {
-        lsp_zero.default_setup,
         lua_ls = function()
           local lua_opts = lsp_zero.nvim_lua_ls()
           require('lspconfig').lua_ls.setup(lua_opts)
