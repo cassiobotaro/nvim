554aaf83928f97fe98d2cda39e9fb417b13d7b58
cassiobotaro <cassiobotaro@gmail.com>
2024-01-17 22:22:53 -0300

    Move toggleterm keys

--- lua/plugins/toggleterm.lua
+++ lua/plugins/toggleterm.lua
@@ -1,10 +1,10 @@
 -- Terminal
 return {
-  'akinsho/toggleterm.nvim',
-  version = '*',
-  config = function()
-    require('toggleterm').setup {
-      open_mapping = [[<leader>t]],
-    }
-  end,
+    'akinsho/toggleterm.nvim',
+    version = '*',
+    config = function()
+        require('toggleterm').setup {
+            open_mapping = [[<c-t>]],
+        }
+    end,
 }
