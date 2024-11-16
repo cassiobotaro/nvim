d5493131a70092eec52c7c36f441f3529ff99cdc
cassiobotaro <cassiobotaro@gmail.com>
2024-11-16 18:12:11 -0300

    fix markdown preview installation

--- lua/plugins/markdown-preview.lua
+++ lua/plugins/markdown-preview.lua
@@ -1,8 +1,9 @@
 return {
-  'iamcco/markdown-preview.nvim',
-  cmd = { 'MarkdownPreviewToggle', 'MarkdownPreview', 'MarkdownPreviewStop' },
-  ft = { 'markdown' },
-  build = function()
-    vim.fn['mkdp#util#install']()
-  end,
+    "iamcco/markdown-preview.nvim",
+    cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
+    ft = { "markdown" },
+    build = function()
+        require("lazy").load { plugins = { "markdown-preview.nvim" } }
+        vim.fn["mkdp#util#install"]()
+    end,
 }
