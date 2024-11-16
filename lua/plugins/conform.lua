67852c063d80f206d568e9667e34f758aced6bb2
cassiobotaro <cassiobotaro@gmail.com>
2024-11-16 19:19:30 -0300

    fix python formatter

--- lua/plugins/conform.lua
+++ lua/plugins/conform.lua
@@ -1,24 +1,23 @@
 return {
-  "stevearc/conform.nvim",
-  event = { "BufWritePre" },
-  cmd = { "ConformInfo" },
+  'stevearc/conform.nvim',
+  event = { 'BufWritePre' },
+  cmd = { 'ConformInfo' },
   config = function()
-    require("conform").setup({
+    require('conform').setup {
       formatters_by_ft = {
-        lua = { "stylua" },
-        python = { "ruff" },
-        go = { "goimports" },
-        bash = { "shfmt" },
-        yaml = { "prettier" },
-        json = { "prettier" },
-        markdown = { "prettier" },
-        toml = { "prettier" },
+        lua = { 'stylua' },
+        go = { 'goimports' },
+        bash = { 'shfmt' },
+        yaml = { 'prettier' },
+        json = { 'prettier' },
+        markdown = { 'prettier' },
+        toml = { 'prettier' },
       },
       format_on_save = {
         -- These options will be passed to conform.format()
         timeout_ms = 500,
-        lsp_format = "fallback",
+        lsp_format = 'fallback',
       },
-    })
+    }
   end,
 }
