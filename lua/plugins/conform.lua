82bf37a59890bfc1b462373c0e3c5558778b3c15
cassiobotaro <cassiobotaro@gmail.com>
2024-11-19 18:18:42 -0300

    add ruff organize imports and update dependencies

--- lua/plugins/conform.lua
+++ lua/plugins/conform.lua
@@ -6,6 +6,7 @@ return {
     require('conform').setup {
       formatters_by_ft = {
         lua = { 'stylua' },
+        python = { 'ruff_format', 'ruff_organize_imports' },
         go = { 'goimports' },
         bash = { 'shfmt' },
         yaml = { 'prettier' },
