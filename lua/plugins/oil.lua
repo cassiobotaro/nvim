d1bc70b87c3b1b10e1c01e29e3389c4faf962bd2
cassiobotaro <cassiobotaro@gmail.com>
2024-12-23 22:40:34 -0300

    Move from neo-tree to oil, change terminal to float...
    ...and also update dependencies.

---
+++ lua/plugins/oil.lua
@@ -0,0 +1,9 @@
+return {
+  'stevearc/oil.nvim',
+  lazy = false,
+  keys = {
+    { '-', '<cmd>Oil<cr>', desc = 'Toggle file explorer' },
+  },
+  dependencies = { 'nvim-tree/nvim-web-devicons' },
+  config = true,
+}
--- lua/plugins/toggleterm.lua
+++ lua/plugins/toggleterm.lua
@@ -1,10 +1,11 @@
 -- Terminal
 return {
-    'akinsho/toggleterm.nvim',
-    version = '*',
-    config = function()
-        require('toggleterm').setup {
-            open_mapping = [[<c-t>]],
-        }
-    end,
+  'akinsho/toggleterm.nvim',
+  version = '*',
+  config = function()
+    require('toggleterm').setup {
+      direction = 'float',
+      open_mapping = [[<c-t>]],
+    }
+  end,
 }
