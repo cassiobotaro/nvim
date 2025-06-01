d3001a4e8a4183b2edb30add2791d42abe60a989
cassiobotaro <cassiobotaro@gmail.com>
2025-06-01 18:51:46 -0300

    add comments for structurizr

--- lua/plugins/comment.lua
+++ lua/plugins/comment.lua
@@ -1,5 +1,10 @@
 -- comment stuff out
 return {
   'numToStr/Comment.nvim',
-  config = true,
+  config = function()
+    require('Comment').setup()
+    local ft = require 'Comment.ft'
+    -- Add filetype specific comment strings
+    ft.set('structurizr', { '//%s', '/*%s*/' })
+  end,
 }
