285c36f036ce8bcedbf4e7fe5c3bd24750faf36e
cassiobotaro <cassiobotaro@gmail.com>
2025-07-15 19:16:28 -0300

    update dependencies

--- lua/plugins/codecompanion.lua
+++ lua/plugins/codecompanion.lua
@@ -2,17 +2,6 @@ return {
   'olimorris/codecompanion.nvim',
   event = 'VeryLazy',
   opts = {
-    adapters = {
-      copilot = function()
-        return require('codecompanion.adapters').extend('copilot', {
-          schema = {
-            model = {
-              default = 'claude-3.7-sonnet',
-            },
-          },
-        })
-      end,
-    },
     strategies = {
       chat = { adapter = 'copilot' },
       inline = { adapter = 'copilot' },
